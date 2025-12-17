3a9aa464ee2f5b76af509d32a107b0d84ffaf07a
GoobusTheNoobus <huand2464@wrdsb.ca>
2025-12-17 17:42:23 -0500

    Refactor Constants class for bitboard representation

--- BlunderFish/src/utils/Constants.java
+++ BlunderFish/src/utils/Constants.java
@@ -1,6 +1,19 @@
 package utils;
 public class Constants {
-    
+
+    public static final int WP_BITBOARD = 0;
+    public static final int WN_BITBOARD = 1;
+    public static final int WB_BITBOARD = 2;
+    public static final int WR_BITBOARD = 3;
+    public static final int WQ_BITBOARD = 4;
+    public static final int WK_BITBOARD = 5;
+
+    public static final int BP_BITBOARD = 6;
+    public static final int BN_BITBOARD = 7;
+    public static final int BB_BITBOARD = 8;
+    public static final int BR_BITBOARD = 9;
+    public static final int BQ_BITBOARD = 10;
+    public static final int BK_BITBOARD = 11;
 
     public static final char WHITE_PAWN_CHAR  = '♙';
     public static final char WHITE_KNIGHT_CHAR = '♘';
@@ -15,8 +28,6 @@ public class Constants {
     public static final char BLACK_ROOK_CHAR   = '♜';
     public static final char BLACK_QUEEN_CHAR  = '♛';
     public static final char BLACK_KING_CHAR   = '♚';
-
-    
 
     public static final long[] ROOK_MAGIC_NUMBERS = {
         0x4080002088104000L,
