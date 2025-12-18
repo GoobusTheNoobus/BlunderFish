0e0f48c9dade15da60f9bdec0e9c9e7416d1aee5
GoobusTheNoobus <huand2464@wrdsb.ca>
2025-12-18 17:17:05 -0500

    Updated Main File

--- BlunderFish/src/utils/Utility.java
+++ BlunderFish/src/utils/Utility.java
@@ -1,26 +1,36 @@
-package utils;
+import board.*;
+import gen.MoveGenerator;
+import utils.*;
 
 
-public class Utility {
-    public static int getSquareIntFromString (String squareString) {
-        char fileC = squareString.charAt(0);
-        char rankC = squareString.charAt(1);
+public class App {
+    public static void main(String[] args) throws Exception {
+        Bitboards.init();
 
-        int file = fileC - 'a';
-        int rank = rankC - '1';
+        final int WARMUP_ITERS = 200000;
+        final int TEST_ITERS = 2_000_000;
+        long x = 0;
 
-        return rank * 8 + file;
-    }
-
-    public static String getStringFromSquareInt(int square) {
-        int file = square & 7;
-        int rank = square >> 3;
-
-        char f = (char)(file + 'a');
-        char r = (char)(rank + '1');
-
-        return "" + f + r;
-    }
-
+        for (int i = 0; i < WARMUP_ITERS; i++) {
+            x = x ^ (i * 31) + (x << 1);
+        }
+        Position p = new Position("8/8/3n4/R2R4/1B3Q2/8/4k3/8 w - - 0 1");
+        Timer.start();
+        
+        for (int i = 0; i < TEST_ITERS; i ++) {
+            // Operations Here
+            MoveGenerator.generatePsuedoLegalMoves(p);
+            MoveGenerator.clearArrays();
+        }
+        System.out.println(p.isSquareAttacked(16, true));
+        
     
+
+
+        Timer.stop();
+        
+        Timer.printAverageTime(TEST_ITERS);
+
+    }
 }
+   
