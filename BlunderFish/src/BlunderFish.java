b6219cca82fdfea0066462e358857b237a4203a9
GoobusTheNoobus <huand2464@wrdsb.ca>
2025-12-20 15:48:55 -0500

    Refactor main method for performance improvements
    Reduced test iterations and optimized move generation process.

--- BlunderFish/src/BlunderFish.java
+++ BlunderFish/src/BlunderFish.java
@@ -8,26 +8,31 @@ public class BlunderFish {
         Bitboards.init();
 
         final int WARMUP_ITERS = 200000;
-        final int TEST_ITERS = 2_000_000;
-        long x = 0;
+        final int TEST_ITERS = 1_000_000;
+        
 
+        Position pos = new Position("8/8/3n4/R2R4/1B3Q2/8/4k3/8 w - - 0 1");
         for (int i = 0; i < WARMUP_ITERS; i++) {
-            x = x ^ (i * 31) + (x << 1);
+            MoveGenerator.generatePseudoLegalMoves(pos);
+            MoveGenerator.clearArrays();;
         }
-        Position p = new Position("8/8/3n4/R2R4/1B3Q2/8/4k3/8 w - - 0 1");
+        
         Timer.start();
+
+
         
         for (int i = 0; i < TEST_ITERS; i ++) {
             // Operations Here
-            MoveGenerator.generatePsuedoLegalMoves(p);
+            MoveGenerator.generatePseudoLegalMoves(pos);
+            
             MoveGenerator.clearArrays();
+            
         }
-        System.out.println(p.isSquareAttacked(16, true));
-        
-    
-
 
         Timer.stop();
+
+        MoveGenerator.generatePseudoLegalMoves(pos);
+        MoveGenerator.printMoveList();
         
         Timer.printAverageTime(TEST_ITERS);
 
